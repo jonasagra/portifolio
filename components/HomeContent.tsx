9a32809aa82d1b40a951360af8f57a8cdab22d5d
jonasagra <jonasagrajp@gmail.com>
2026-06-26 22:35:14 -0300

    fixing footer

--- components/HomeContent.tsx
+++ components/HomeContent.tsx
@@ -243,7 +243,7 @@ export default function HomeContent() {
           Criado por <span className="text-white tracking-widest uppercase">Jonas Agra</span> © 2026
           <br />
           <p className="text-zinc-500 font-medium text-sm">
-          E-mail: <span className="text-white tracking-widest uppercase">jnasagra@gmail.com</span> © 2026
+          E-mail: <span className="text-gray-500 font-normal text-xs">jnasagra@gmail.com</span>
           <br />
           <span className="text-gray-500 font-normal text-xs">Todos os direitos reservados.</span>
           </p>
